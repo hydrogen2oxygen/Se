ab5260881dca3baba7dd92d9080f9f1352c42552
hydrogen2oxygen <luxuspeter@googlemail.com>
2021-11-02 14:42:53 +0100

    Groups / Selenium 4.0

--- src/main/java/net/hydrogen2oxygen/se/Group.java
+++ src/main/java/net/hydrogen2oxygen/se/Group.java
@@ -1,15 +1,59 @@
 package net.hydrogen2oxygen.se;
 
+import net.hydrogen2oxygen.se.exceptions.PreconditionsException;
+
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
-public class Group {
+public class Group implements IAutomation {
 
+    private static Logger logger = LogManager.getLogger(Group.class);
+    private Se se;
+    private String groupName;
     private List<IAutomation> automationList = new ArrayList<>();
+
+    public Group(Se se, String groupName) {
+        this.se = se;
+        this.groupName = groupName;
+    }
 
     public void add(IAutomation automation) {
         automationList.add(automation);
     }
 
+    @Override
+    public void checkPreconditions() throws PreconditionsException {
 
+        logger.info("Running group automation " + groupName);
+
+        for (IAutomation automation : automationList) {
+            try {
+                automation.checkPreconditions();
+            } catch (PreconditionsException e) {
+                logger.error(e);
+            } catch (Exception e) {
+                logger.error(e);
+            }
+        }
+    }
+
+    @Override
+    public void run() throws Exception {
+        for (IAutomation automation : automationList) {
+            try {
+                automation.run();
+                automation.cleanUp();
+            } catch (Exception e) {
+                logger.error(e);
+            }
+        }
+    }
+
+    @Override
+    public void cleanUp() throws Exception {
+        // finally, a group always close the driver, ALWAYS
+        se.getWebDriver().close();
+    }
 }
